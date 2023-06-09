ec3d2b6c218865e03a8970669e808f2327d39e98
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-06-09 16:30:56 +0200

    head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="Home">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Twitter Clone</title>
-        <link rel="favicon" type="image/x-icon" href="./public/favicon.ico" />
-      </head>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>Twitter Clone</h1>
-        
+
       </main>
     </div>
   )
